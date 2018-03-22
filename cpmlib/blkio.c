4455240b8f45be0614a79e4fccdc69953e821735
none <none>
2018-03-22 11:29:01 -0700

    add TODO, remove unusable multi-sector I/O stuff

--- cpmlib/blkio.c
+++ cpmlib/blkio.c
@@ -29,12 +29,9 @@
 #include        "portab.h"
 #include        "osif.h"
 #include        "osiferr.h"
-#include        "osattr.h"
 #include        "prototypes.h"
 
 #define MIN(x,y) ( ((x) < (y)) ? (x) : (y) )
-
-static int __cpmrv = 0; // never set / used XXX TODO remove
 
 LONG
 _blkio(
@@ -49,10 +46,8 @@ _blkio(
         LONG    seccnt;                         /* # sectors processed      */
         WORD    xuser;                          /* old user number          */
         WORD    retcode;                        /* return code from BDOS    */
-        WORD    Used_Multisec;                  /* flag for resetting msecio*/
                                                 /****************************/
         seccnt = 0;                             /* Zero count initally      */
-        Used_Multisec = 0;                      /* Haven't used it yet      */
         xuser = _chkuser(ccbp->user);           /* Check for changed user # */
         nsecs = 1;                              /* default: xfer singly     */
                                                 /*                          */
@@ -66,27 +61,12 @@ _blkio(
                 ccbp->fcb.record = sector << 8; /* Set new record number    */
 #endif          /*==========================================================*/
 
-                                                /*   Multi Sector I/O xfer? */
-                if( (count > 1  &&  os_multisectorio ) || nsecs > 1 ) 
-                {                               /* yes!                     */
-                    if( os_oldmultisec )
-                         nsecs = MIN(16,count); /* Older version of msecio  */
-                    else nsecs = MIN(128,count);/* Later version of msecio  */
-                    ++Used_Multisec;            /* Flag: need to reset      */
-                    __OSIF(F_MULTISEC,nsecs);   /* make the call            */
-                }                               /*******                    */
                                                 /****************************/
                 retcode = __OSIF(bdosfunc,&(ccbp->fcb));/* do the read/write*/
                                                 /****************************/
                 if(retcode != 0)
                 {                               /****************************/
-                    if(nsecs>1)                 /* were we trying multi-sec?*/
-                    {                           /* yes! do error handling   */
-                      if( os_oldmultisec )      /* Older msecio?            */
-                         nsecs = __cpmrv >> 12; /* adjust nsecs read like so*/
-                      else nsecs = __cpmrv >> 8;/* later versions: use AH   */
-                    }                           /***                        */
-                    else nsecs = 0;             /* ow. assume 0 secs read   */
+                    nsecs = 0;                  /* ow. assume 0 secs read   */
                     seccnt += nsecs;            /* Return nfg               */
                     break;                      /* Return from bottom       */
                 }                               /****************************/
@@ -97,7 +77,5 @@ _blkio(
                 buffer += nsecs*SECSIZ;         /* Increment address        */
         }                                       /****************************/
         _uchkuser(ccbp->user,xuser);            /* Reset user # if needed   */
-        if( Used_Multisec )                     /* Need to reset msecio?    */
-            __OSIF(F_MULTISEC,1);               /*   then do so...          */
         return(seccnt);                         /* All is OK                */
 }                                               /****************************/
